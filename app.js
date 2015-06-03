b8fa1675c6a7027465503ce28879f667157deff9
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2015-06-03 01:59:18 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,38 @@
+var bcrypt = require('bcrypt');
+var jwt = require('jsonwebtoken');
+
+var token;
+var mySalt;
+
+var decode = function() {
+    console.log(jwt.decode(token));
+};
+
+var verify = function() {
+    //mySalt = 'heehh';
+
+    jwt.verify(token, mySalt, function(err, decoded) {
+        if (err) {
+            console.log(err);
+            return
+        }
+
+        console.log(decoded);
+    });
+};
+
+bcrypt.genSalt(10, function(err, salt) {
+    mySalt = salt;
+    
+    token = jwt.sign({
+        type: 'general',
+        value: 'mybiggestsecret'
+    }, mySalt, { 
+        algorithm: 'HS512' ,
+        expiresInSeconds: 5
+    });
+
+    //decode();
+    //verify();
+    setTimeout(verify, 6000);
+});
